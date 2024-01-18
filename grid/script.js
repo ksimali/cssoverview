21393bf7edfdcab6dc97034ad64b74acb2eb0d84
ksimali <cassim.ali@outlook.fr>
2024-01-18 16:25:16 -0500

    Setting up js function: -Add ajouterItem() function -Add afficherFacture() function

---
+++ grid/script.js
@@ -0,0 +1,35 @@
+
+/* fonction d'ajout d'article*/
+function ajouterItem(produit, quantite, prix){
+    var nouvelleLigne = "<tr>";
+    nouvelleLigne += "<td>" + produit +"</td>";
+    nouvelleLigne += "<td>"+ quantite+"</td>";
+    nouvelleLigne += "<td>"+ prix +"</td>";
+    nouvelleLigne += "</tr>";
+
+    var ancienContenu = document.getElementById("corpsTableau").innerHTML;
+    var nouveauContenu = ancienContenu + nouvelleLigne;
+    document.getElementById("corpsTableau").innerHTML = nouveauContenu;
+
+    var nombreItemPanier = parseInt(document.getElementById("idItem").innerHTML);
+    nombreItemPanier += parseInt(quantite);
+    document.getElementById("idItem").innerHTML = nombreItemPanier;
+}
+
+// fonction affichage du tableau
+function afficherFacture(){
+    if(document.getElementById("zoneContenuFacture").style.display == "none"){
+        document.getElementById("zoneContenuItem").style.display = "none";
+        document.getElementById("zoneContenuFacture").style.display = "block";
+
+    }else{
+        document.getElementById("zoneContenuItem").style.display = "grid";
+        document.getElementById("zoneContenuFacture").style.display = "none";
+    }
+    
+
+}
+// 
+const panier = document.getElementById("panier");
+// Ajout d'un event listener qui s'active quand l'utilisateur clique sur l'élement id panier
+panier.addEventListener("click", afficherFacture);
